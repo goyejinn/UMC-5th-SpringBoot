558dc654da0206b3bee597f7bf45f7a70de8b1a4
Yejin <kyj5840kyj@duksung.ac.kr>
2023-11-03 23:59:37 +0900

    initialize

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,28 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String title;
+
+    private Float score;
+}
